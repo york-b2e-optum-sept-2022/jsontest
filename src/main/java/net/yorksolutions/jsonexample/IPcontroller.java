f1c07fb3c7c2c0f845c26b51b0ac0e7768849567
yorkmacbook017 <ismailsheikhomar126@gmail.com>
2022-10-22 16:04:49 -0500

    got how to get date functioning

--- src/main/java/net/yorksolutions/jsonexample/IPcontroller.java
+++ src/main/java/net/yorksolutions/jsonexample/IPcontroller.java
@@ -3,6 +3,9 @@ package net.yorksolutions.jsonexample;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -21,8 +24,25 @@ public class IPcontroller {
     }
 
     @GetMapping("/date")
-    public String genHeader() {
+    public String gendate() {
         return this.jsonService.getdate();
     }
+
+    @GetMapping("/header")
+    public HashMap genHeader(HttpServletRequest request){
+        return this.jsonService.getHeader(request) ;
+    }
+
+    @GetMapping("/echo/**")
+    public HashMap getecho(HttpServletRequest request) {
+        return this.jsonService.getecho(request);
+
+    }
+
+    @GetMapping("/md5")
+    public HashMap getMD5(HttpServletRequest request) {
+        return this.jsonService.getMD5();
+    }
+
 
 }
--- src/main/java/net/yorksolutions/jsonexample/JsonService.java
+++ src/main/java/net/yorksolutions/jsonexample/JsonService.java
@@ -2,9 +2,13 @@ package net.yorksolutions.jsonexample;
 
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
+import java.util.Enumeration;
+import java.util.HashMap;
 
 
 @Service
@@ -26,6 +30,43 @@ public class JsonService {
         this.Tdate = String.valueOf(LocalDateTime.now());
         return "Date is " + Tdate;
     }
+
+public HashMap getecho(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String[] tokenList = uri.split("/");
+
+        HashMap map = new HashMap();
+        for (int i = 2; i < tokenList.length; i+= 2) {
+            String key = tokenList[i];
+
+            String value = "";
+            if (i + 1 < tokenList.length) {
+                value = tokenList[i + 1];
+            }
+
+            map.put(
+                    key,
+                    value
+            );
+    }
+        return map;
+
+}
+
+public HashMap getMD5() {
+        return new HashMap();
+}
+
+public HashMap getHeader(HttpServletRequest request) {
+        HashMap map = new HashMap();
+    Enumeration<String> headerNameList = request.getHeaderNames();
+    while(headerNameList.hasMoreElements()) {
+        String headerName = headerNameList.nextElement();
+        String headerData = request.getHeader(headerName);
+        map.put(headerName, headerData);
+    }
+        return map;
+}
 
 
 
